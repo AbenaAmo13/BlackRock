171cfebaff38ec71f2bfcb32b2343bf2f1c70888
AbenaAmo13 <60615703+AbenaAmo13@users.noreply.github.com>
2022-02-05 15:44:24 +0000

    feat : Returns a collection of portfolio objects.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,16 +12,16 @@ import java.util.regex.Pattern;
 
 public class Main {
 
-    class Portfolio {
-        String porfolioName;
-        String positionName;
-        Date startDate;
-        Date endDate;
-        boolean positionOpen;
+    static class Portfolio {
+        private String portfolioName;
+        private String positionName;
+        private String startDate;
+        private String endDate;
+        private boolean positionOpen;
 
-        public Portfolio(String portfolioName, String positionName, Date startDate, Date endDate, boolean positionOpen) {
-            this.porfolioName = portfolioName;
-            this.positionName = positionName;
+        public Portfolio(String portfolioName, String portfolioPositionName, String startDate, String endDate) {
+            this.portfolioName = portfolioName;
+            this.positionName = portfolioPositionName;
             this.startDate = startDate;
             this.endDate = endDate;
             this.positionOpen = false;
@@ -42,46 +42,58 @@ public class Main {
 
     public static void processInput(String dateString, Collection<Portfolio> portfolios) {
         // Access your code here. Feel free to create other classes as required
-
     }
-
     private static Collection<Portfolio> getPortfolios(BufferedReader in) throws IOException {
         String startDate = "";
         String endDate = "";
         String portfolioName = "";
         String portfolioPositionName = "";
+        boolean portfolioInformationRetrieved = false;
         Collection<Portfolio> portfolios = new ArrayList<Portfolio>();
         // expand this class to read the portfolios. Feel free to create other classes as required
-
-
         String line;
         while ((line = in.readLine()) != null) {
-            Date checkDate = new Date();
-            //Separate the lines by portfoliopositions
+            //Date checkDate = new Date();
+            //Separate the lines by portfolio positions
             String portfolioPositions[] = line.split(",");
             for (int i = 0; i < portfolioPositions.length; i++) {
                 //This will split the portfolio information to position name, starting date and ending date.
                 String portfolioInformation = portfolioPositions[i];
                 String portfolioDetails[] = portfolioInformation.split(":");
-                //First portfolio position will contain the name of the portfolio as well as the position name s
-                // separated by a | hence should be a special case
-                    String portfolioNameAndPosition = portfolioDetails[0];
-                    int portfolioNameIndex = portfolioNameAndPosition.indexOf("|");
-                    portfolioName = portfolioNameAndPosition.substring(0, portfolioNameIndex);
-                    //Get the positionName by getting the substring that comes after the index.
-                    portfolioPositionName = portfolioNameAndPosition.substring(portfolioNameIndex + 1);
-                startDate = portfolioDetails[1];
-                endDate = portfolioDetails[2];
+                for (int k = 0; k < portfolioDetails.length; k++) {
+                    portfolioInformationRetrieved = false;
 
+                    switch (k) {
+                        case 0:
+                            //First portfolio position will contain the name of the portfolio as well as the position name
+                            // separated by a | hence should be a special case
+                            String portfolioNameAndPosition = portfolioDetails[k];
+                            int portfolioNameIndex = portfolioNameAndPosition.indexOf("|");
+                            if (portfolioNameIndex != -1) {
+                                portfolioName = portfolioNameAndPosition.substring(0, portfolioNameIndex);
+                                //Get the positionName by getting the substring that comes after the index.
+                                portfolioPositionName = portfolioNameAndPosition.substring(portfolioNameIndex + 1);
+                            } else {
+                                //Other information just contain the portfiliopositions
+                                portfolioPositionName = portfolioDetails[k];
+                            }
+                            break;
+                        case 1:
+                            startDate = portfolioDetails[k];
+                            break;
+                        case 2:
+                            endDate = portfolioDetails[k];
+                            portfolioInformationRetrieved = true;
 
-                //String portfolioPosition=
-                //String segments[] = line.split(":");
-                //System.out.println(Arrays.toString(portfolioPositions));
+                            break;
+                    }
+                    //Once you have retrieved all the portfolio information make a portfolio object
+                    if (portfolioInformationRetrieved) {
+                        Portfolio currentPortfolio = new Portfolio(portfolioName, portfolioPositionName, startDate, endDate);
+                        portfolios.add(currentPortfolio);
+                    }
+                }
             }
-            System.out.println(portfolioName);
-            System.out.println(portfolioPositionName);
-            System.out.println(startDate);
-            System.out.println(endDate);
         }
         return portfolios;
     }
